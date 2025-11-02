08395ad44184a5d32bb5de9e7998a3a353a64e79
NiallEHunt <3974381+NiallEHunt@users.noreply.github.com>
2025-11-02 19:46:14 +0000

    Add some pools and player moves faster in water

--- Assets/Niall Test Assets/PlayerMovement.cs
+++ Assets/Niall Test Assets/PlayerMovement.cs
@@ -2,13 +2,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    
+    public float waterSpeed = 5f;
+    public float sandSpeed = 2f;
+
+    private float currentSpeed;
+    // private bool isInWater = false;
+
+    void Start()
+    {
+        currentSpeed = sandSpeed;
+    }
+
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        transform.position += new Vector3(moveX, moveY, 0) * moveSpeed * Time.deltaTime;
+        transform.position += new Vector3(moveX, moveY, 0) * currentSpeed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            // isInWater = true;
+            currentSpeed = waterSpeed;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            // isInWater = false;
+            currentSpeed = sandSpeed;
+        }
     }
 }
