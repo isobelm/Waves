693b6baded311041c1401a86a27e175af138d059
Rory Kelly <rorykelly47@gmail.com>
2025-11-03 19:04:54 +0000

    Circle sprite position resets on collision with enemy

--- Assets/RoryTestAssests/OnCollisionCrab.cs
+++ Assets/RoryTestAssests/OnCollisionCrab.cs
@@ -2,14 +2,7 @@ using UnityEngine;
 
 public class OnCollisionCrab : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
--- Assets/RoryTestAssests/RoryMoveScript.cs
+++ Assets/RoryTestAssests/RoryMoveScript.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class RoryMoveScript : MonoBehaviour
 {
-    public float moveSpeed = 10;
+    public float moveSpeed;
+    public static float START_POSITION_X = 0;
+    public static float START_POSITION_Y = 0;
+    public OnCollisionCrab onCollisionCrab;
+    bool resetCrab;
 
     void Start()
     {
-
+        moveSpeed = 10;
+        //transform.Translate(new Vector2(START_POSITION_X, START_POSITION_Y));
+        transform.localPosition = new Vector2(0, 0);
+        resetCrab = false;
     }
-    
+
     void Update()
     {
+        if (resetCrab) {
+            transform.localPosition = new Vector2(START_POSITION_X, START_POSITION_Y);
+            resetCrab = false; 
+        }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
@@ -26,6 +38,23 @@ public class RoryMoveScript : MonoBehaviour
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            resetCrab = true;
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            resetCrab = true;
         }
     }
 }
